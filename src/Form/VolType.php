a90c05d9551289af52ed9e971df2460e99b8ed0c
Mateo LAPETRE <m.lapetre-sarthou@lprs.fr>
2025-05-07 10:38:17 +0200

    fix: ref_pilote

--- src/Form/VolType.php
+++ src/Form/VolType.php
@@ -2,8 +2,15 @@
 
 namespace App\Form;
 
+use App\Entity\Utilisateur;
 use App\Entity\Vol;
+use App\Repository\UtilisateurRepository;
+use Doctrine\ORM\QueryBuilder;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,11 +21,28 @@ class VolType extends AbstractType
         $builder
             ->add('villeDepart')
             ->add('villeArrive')
-            ->add('dateDepart')
-            ->add('heureDepart')
-            ->add('prixBilletInitial')
+            ->add('dateDepart',DateType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('heureDepart',TimeType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('prixBilletInitial',IntegerType::class, [
+                "attr" =>[
+                    "min"=> 0,
+                    "max"=> 500
+                ]
+            ])
             ->add('refAvion')
-            ->add('refPilote')
+            ->add('refPilote',EntityType::class,
+            [
+                "class" => Utilisateur::class,
+                'query_builder' => function (UtilisateurRepository $er): QueryBuilder {
+                    return $er->createQueryBuilder('u')
+                        ->where('u.roles LIKE :role')
+                        ->setParameter("role", "%ROLE_PILOTE%");
+                },
+            ])
         ;
     }
 
